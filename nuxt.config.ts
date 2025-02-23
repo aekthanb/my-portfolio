df1a123a0bf4316489eb436e67a43e6edbe354e0
KERORO\keroro <aekthan.b>
2025-02-24 05:03:22 +0700

    fig config github

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-02-23",
+  target: "static",
+  router: {
+    base: "/my-portfolio/", 
+  },
   devtools: { enabled: false },
-  css: ["~/assets/css/main.css", "uikit/dist/css/uikit.min.css",
-    'nprogress/nprogress.css', '~/assets/css/nprogress.css'],
+  css: [
+    "~/assets/css/main.css",
+    "uikit/dist/css/uikit.min.css",
+    "nprogress/nprogress.css",
+    "~/assets/css/nprogress.css",
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
