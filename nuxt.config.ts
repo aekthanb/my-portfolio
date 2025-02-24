142e762bebb52f3a9cd550a19c7fd422ff35059b
KERORO\keroro <aekthan.b>
2025-02-24 23:50:02 +0700

    change config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-02-23",
   devtools: { enabled: false },
-  css: ["~/assets/css/main.css", "uikit/dist/css/uikit.min.css",
-    'nprogress/nprogress.css', '~/assets/css/nprogress.css'],
+  css: [
+    "~/assets/css/main.css",
+    "uikit/dist/css/uikit.min.css",
+    "nprogress/nprogress.css",
+    "~/assets/css/nprogress.css",
+  ],
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/x-icon", href: "/personal.ico" }],
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
